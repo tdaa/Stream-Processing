f659347d3a93d527a62ae099648ed1b0f52a5076
jhbb <hugo.b.brandao@gmail.com>
2017-05-14 18:57:27 +0100

    Add a prototype of the node function to the controller

--- controller.c
+++ controller.c
@@ -7,6 +7,9 @@
 #include <stdlib.h>
 #include <signal.h>
 
+#define READ 0
+#define WRITE 1
+
 pid_t nodes[10];
 
 ssize_t readln(int fildes, void *buf, size_t nbyte){
@@ -17,6 +20,13 @@ ssize_t readln(int fildes, void *buf, size_t nbyte){
         i++;
 	}
 	return i;
+}
+
+void processInput(char **result, char *buf, char *divider){
+	int i;
+	for(result[i=0]= strtok(buf, divider); result[i] != NULL; result[i] = strtok(NULL, divider)){
+        i++;
+    }
 }
 
 int getNodeid(char *buf){
@@ -36,31 +46,67 @@ int getNodeid(char *buf){
 }
 
 int main(){
-    int i=0, id;
+    int i=0, id, infd, outfd,  nonamepipein[2], nonamepipeout[2];
     for(i=0;i<10;nodes[i++]=-1);
-    char buf[4096];
-    pid_t p;
+    char buf[4096], *in, *out;
+	char *input[4096];
+    pid_t p, readlnp, dupexecp, writep;
     while((i = readln(0, buf, 4096)) > 0){
-        if(!strncmp(buf, "node", 4)){
-            p = fork();
+		processInput(input, buf, " ");
+        if(!strncmp(input[0], "node", 4)){
+			id = atoi(input[1]);
+			in = strcat("in", input[1]);
+			out = strcat("out", input[1]);
+			mkfifo(in, 0666);
+			mkfifo(out, 0666);
+			p = fork();
             if(!p){
+				infd = open(in, O_RDONLY);
+				outfd = open(out, O_WRONLY);
+				pipe(nonamepipein);
+				pipe(nonamepipeout);
+				readlnp = fork();
+				if(!readlnp){
+					close(nonamepipein[READ]);
+					while((i = readln(infd, buf, 4096)) > 0){
+						write(nonamepipein[WRITE], buf, 4096);
+					}
+				}
+				else{
+					dupexecp = fork();
+					if(!dupexecp){
+						dup2(nonamepipein[READ], 0);//dup do std in para o pipe in
+						close(nonamepipein[READ]);
+						dup2(nonamepipeout[WRITE], 1);//dup do std out para o pipe out
+						close(nonamepipeout[WRITE]);
+						execv(strcat("./", input[2]), &input[3]);
+					}
+					else{
+						writep = fork();
+						if(!writep){
+							close(nonamepipeout[WRITE]);
+							while((i = readln(nonamepipeout[READ], buf, 4096))){
+								write(outfd, buf, 4096);
+							}
+						}
+					}
+				}
                 kill(getpid(), SIGTERM);
             }
             else{
-				id = getNodeid(buf);
                 nodes[id] = p;
             }
         }
-        else if(!strncmp(buf, "connect", 7)){
+        else if(!strncmp(input[0], "connect", 7)){
             printf("You typed connect!\n");
         }
-        else if(!strncmp(buf, "disconnect", 10)){
+        else if(!strncmp(input[0], "disconnect", 10)){
             printf("You typed disconnect!\n");
         }
-        else if(!strncmp(buf, "inject", 6)){
+        else if(!strncmp(input[0], "inject", 6)){
             printf("You typed inject!\n");
         }
-        else if(!strncmp(buf, "print", 5)){
+        else if(!strncmp(input[0], "print", 5)){
             for(i=0;i<10;i++){
                 if(nodes[i]!=-1) printf("I EXIST: %d\n", nodes[i]);
             }
