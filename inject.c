2be5b06d8c190630101e1fce3355b2ad837262ac
a-sac <asergioalvesc@gmail.com>
2017-06-02 00:51:51 +0100

    inject is now operacional

--- inject.c
+++ inject.c
@@ -13,13 +13,9 @@
 
 int main(int argc, char** argv){
     int i, nonamepipeout[2];
-    char* id = concat("in", argv[1]);
-    char* args;
+    char* id = "";
+    id = concat("in", argv[1]);
     char buf[PIPE_BUF];
-    for(i=3; i<argc; i++){
-        args = concat(args, argv[i]);
-        args = concat(args, " ");
-    }
     pid_t p;
     pipe(nonamepipeout);
     p = fork();
@@ -27,14 +23,16 @@ int main(int argc, char** argv){
         close(nonamepipeout[0]);
         dup2(nonamepipeout[1], 1);
         close(nonamepipeout[1]);
-        execv(argv[2], &args);
+        execv(argv[2], &argv[2]);
     }
     close(nonamepipeout[1]);
     int infd = open(id,	O_WRONLY);
     i = readln(nonamepipeout[0], buf, PIPE_BUF);
 	write(infd, buf, i);
 	fsync(infd);
+    close(nonamepipeout[0]);
     close(infd);
+    kill(p,SIGTERM);
 
     return 0;
 }
